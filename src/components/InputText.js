47420f03524beec18edcefe696cfc530344edad6
tejasgawali4 <tejas.gawali4@gmail.com>
2022-10-29 15:22:55 +0530

    init

---
+++ src/components/InputText.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const InputText = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default InputText
+
+const styles = StyleSheet.create({})
